e0bc984dbf287c28c2aa938eb4bdd83734fad9d4
Wesley Farias <wesley_Farias7@outlook.com>
2026-06-15 17:55:20 -0300

    Publica site estatico

---
+++ _next/static/chunks/app/layout-926c05afb915bc4b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1031:()=>{},1290:()=>{},4380:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1290,23)),Promise.resolve().then(n.t.bind(n,1031,23))}},e=>{e.O(0,[0,741,441,255,358],()=>e(e.s=4380)),_N_E=e.O()}]);
